5fd81b226ce65b2d9e1de96b24a724ce6cb7731e
Pherpher089 <Christutor089@gmail.com>
2020-09-26 21:38:15 -0700

    Added two components for the other pages. This is kindof redundant but I wanted to keep the Heirarchy the same in case we scale.

--- the-blown-save/src/App.js
+++ the-blown-save/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from "./components/Navbar.js";
 import Footer from './components/Footer.js';
+import HomePage from './components/HomePage.js';
 
 function App() {
   return (
     <Router>
       <Navbar/>
-      <Route
+      <Route path="/" render={HomePage}/>
       <Footer/>
     </Router>
   );
