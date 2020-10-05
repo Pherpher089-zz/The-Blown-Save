6346835449cdffb8206baae0c717750b9f2b1736
Pherpher089 <Christutor089@gmail.com>
2020-10-04 23:31:27 -0700

    working on the nav bar. Added mixin file and reset file

--- the-blown-save/src/components/Navbar.js
+++ the-blown-save/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
+
+import TitleLogo from '../imgs/TitleLogoInverted.png'
+
 function Navbar(){
     return (
         <div className="nav-container" >
-            <h1>The Blown Save</h1>
+            <img src={TitleLogo} alt='The Blown Save' className='nav-logo'/>
             <div className="nav-buttons">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
