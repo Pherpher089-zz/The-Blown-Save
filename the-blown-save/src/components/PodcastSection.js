ac8b4691a00d457f752cfb04baa49455012264b3
Pherpher089 <Christutor089@gmail.com>
2020-10-07 01:02:08 -0700

    working on podcast section

--- the-blown-save/src/components/PodcastSection.js
+++ the-blown-save/src/components/PodcastSection.js
@@ -3,7 +3,21 @@ import React from 'react';
 function PodcastSection(props) {
     return (
         <div className='podcast-container'>
-            <h1>Podcast Section</h1>
+            <div className='player-container'>
+                <div className='player-inner'>
+                    <div className="player-content">
+                        <img src="" alt=""/>
+                        <div className='player-text-container'>
+                            <p>Date</p>
+                            <div className='title-and-date'>
+                                <h1>Title</h1>
+                                <p>date</p>
+                            </div>
+                            <p>Description - Here is an example of what a description will look like. I think it's going to need at least a few sentences.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
