fe2b1d63b37386be732f33f547e6ad08fd53257f
Pherpher089 <Christutor089@gmail.com>
2020-10-13 01:00:39 -0700

    working on a data object for the merch section

---
+++ the-blown-save/src/data/merch_data.js
@@ -0,0 +1,56 @@
+import hoodie_00 from '../imgs/merch/hoodie_00.jpg';
+import Hoodie_01 from '../imgs/merch/hoodie_01.jpg';
+import Leggings_00 from '../imgs/merch/leggings_00.jpg';
+import Leggings_01 from '../imgs/merch/leggings_01.jpg';
+import Mask_00 from '../imgs/merch/mask_00.jpg';
+import Mask_01 from '../imgs/merch/mask_01.jpg';
+import Mug_00 from '../img/merch/mug_00.jpg';
+import Mug_01 from '../img/merch/mug_01.jpg';
+import Phone_case_00 from '../imgs/merch/phone_case_00.jpg';
+import Phone_case_01 from '../imgs/merch/phone_case_01.jpg';
+import Pillow_00 from '../imgs/merch/pillow_00.jpg';
+import Sticker_00 from '../img/merch/pillow_00.jpg';
+import Tank_00 from '../imgs/merch/tank_00.jpg';
+import Tank_01 from '../imgs/merch/tank_10.jpg';
+import Tee_00 from '../imgs/merch/tee_00.jpg';
+import Tee_01 from '../imgs/merch/tee_01.jpg';
+import Tee_02 from '../imgs/merch/tee_02.jpg';
+import Tee_03 from '../imgs/merch/tee_03.jpg';
+
+
+export const merch_data = {
+    Hoodie_00: {
+        img: Hoodie_00,
+        name: "Blown Save V2",
+        type: "Premium Pullover Hoodie",
+        price: 44.99
+
+    },
+    Hoodie_01: {
+        img: Hoodie_01,
+        name: "Blown Save V2",
+        type: "Unisex Zip Hoodie ",
+        price: 45.99
+
+    },
+    Leggings:{
+        img: Leggings_00,
+        name: "Blown Save V2",
+        type: "Premium Pullover Hoodie",
+        price: 44.99
+
+    },
+    Leggings_01: {
+        img: Leggings_01,
+        name: "Blown Save V2",
+        type: "Leggings",
+        price: 44.99
+    },
+    Mask_00: {
+        img: Mask_00,
+        name: "Blown Save V2",
+        type: "Leggings",
+        price: 44.99
+
+
+}
