6915c3b96480466093b55129d191ecfcfcda16d1
Pherpher089 <Christutor089@gmail.com>
2020-10-14 21:50:45 -0700

    Styling the contacorm

--- the-blown-save/src/components/ContactSection.js
+++ the-blown-save/src/components/ContactSection.js
@@ -1,29 +1,29 @@
 import React from 'react';
-
+import BackGround from '../imgs/contact_background_redLaces.png'
 function ContactSection (props) {
     return(
         <div className='contact-container'>
             <div className='form-container'>
                 <h1 className='contact-title'>Contact Us</h1>
                 <form>
-                <div className='name-email'>
-                    <div className='title-field'>
-                        <h2>Name</h2>
+                    <div className='name-email'>
+                        <div className='title-field'>
+                            <h2>Name</h2>
+                            <input type="text"/>
+                        </div>
+                        <div className='title-field'>
+                            <h2>Emial</h2>
+                            <input type="text"/>
+                        </div>
+                    </div>
+                    <div className='title-field message'>
+                        <h2>Message</h2>
                         <input type="text"/>
                     </div>
-                    <div className='title-field'>
-                        <h2>Emial</h2>
-                        <input type="text"/>
+                    <div className='message-buttons'>
+                        <button>Submit</button>
+                        <button>Reset</button>
                     </div>
-                </div>
-                <div className='title-field message'>
-                    <h2>Message</h2>
-                    <input type="text"/>
-                </div>
-                <div className='message-buttons'>
-                    <button>Submit</button>
-                    <button>Reset</button>
-                </div>
                 </form>
                 {/* <div className='social-media'>
                     <img src="" alt=""/>
