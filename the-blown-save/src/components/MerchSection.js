4c19dd6062da91a8cd597eb86240584ae7bfde45
Pherpher089 <Christutor089@gmail.com>
2020-10-17 16:06:56 -0700

    finnished basic carousel functionality

--- the-blown-save/src/components/MerchSection.js
+++ the-blown-save/src/components/MerchSection.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MerchItem from './MerchItem.js'
 import merchData from '../data/merch_data.js'
+import LeftArrow from '../imgs/left_arrow.png'
+import RightArrow from '../imgs/right_arrow.png'
 
 function MerchSection() {
     const merch = merchData
+    const [x, setX] = useState(0)
+    let GoLeft =()=>{
+        setX(x+100)
+    }
+    let GoRight = ()=>{
+        console.log(x)
+        setX(x-100)
+    }
     return(
         <div className='merch-container'>
             <div className='title-section'>
                 <h1>Get your hands on exclusive Blown Save merch</h1>
             </div>
             <div className="carousel">
-                {merch.map(item => (
-                    <MerchItem item={item} />
-                ))}
+                    {merch.map((item, index) => (
+                        <div className='item-container' style={{transform: `translateX(${x}%)`}}>
+                            <MerchItem item={item} />
+                        </div>
+                    ))}
             </div>
+            <button id='goLeft' onClick={GoLeft}>Left</button>
+            
+            <button id='goRight' onClick={GoRight}>Right</button>
+            {/* <img src={LeftArrow} alt="left" id='goLeft' onClick={GoLeft} />
+            <img src={RightArrow} alt="right" id='goRight' onClick={GoRight}/> */}
         </div>
     )
 }
--- the-blown-save/src/index.js
+++ the-blown-save/src/index.js
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App className='app' />
   </React.StrictMode>,
   document.getElementById('root')
 );
