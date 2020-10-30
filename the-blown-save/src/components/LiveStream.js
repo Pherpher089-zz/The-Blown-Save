5538ef94af07d5796bb058772eb13229a1efec99
Pherpher089 <Christutor089@gmail.com>
2020-10-30 00:26:20 -0700

    styled the offline compoent

--- the-blown-save/src/components/LiveStream.js
+++ the-blown-save/src/components/LiveStream.js
@@ -19,7 +19,8 @@ function LiveStream() {
 function OfflineComponent() {
     return (
         <div className='offline-component' style={{ 'backgroundImage': `url(${WhiteNoise})` }}>
-            <h1>Sorry, we're offline right now. {'\n'}Check out our live stream here at theblownsavepodcast.com at 8:00 PM Pacific October 30th 2020!</h1>
+            <h1>Sorry, we're offline right now. {'\n'}Check out our live stream here at 8:00 PM Pacific October 30th 2020! </h1>
+            <p>Subscribe <a href='https://www.youtube.com/channel/UCXhaOiubzakuGc4YGj_0XSw' target="_blank" >here</a> to make sure you don't miss out</p>
         </div>
     )
 }
